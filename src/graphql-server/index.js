6e27739578af6eba71572b76a1f50c528bc6fa30
Håkon Krogh <hakonhk@gmail.com>
2021-05-12 09:31:48 +0200

    wip

--- src/graphql-server/index.js
+++ src/graphql-server/index.js
@@ -1,11 +1,32 @@
+const { ApolloGateway } = require("@apollo/gateway");
+
 const createContext = require("./create-context");
 const resolvers = require("./resolvers");
 const typeDefs = require("./type-defs");
 
+const CRYSTALLIZE_TENANT_IDENTIFIER = process.env.CRYSTALLIZE_TENANT_IDENTIFIER;
+
 module.exports = function createGraphqlServerConfig({ apiPathPrefix = "" }) {
   const context = createContext({ apiPathPrefix });
 
+  const gateway = new ApolloGateway({
+    serviceList: [
+      {
+        name: "catalogue",
+        url: `https://api.crystallize.com/${CRYSTALLIZE_TENANT_IDENTIFIER}/catalogue`,
+      },
+      {
+        name: "search",
+        url: `https://api.crystallize.com/${CRYSTALLIZE_TENANT_IDENTIFIER}/search`,
+      },
+    ],
+    introspectionHeaders: {
+      Authorization: "Bearer abc123",
+    },
+  });
+
   return {
+    gateway,
     context,
     resolvers,
     typeDefs,
@@ -16,5 +37,7 @@ module.exports = function createGraphqlServerConfig({ apiPathPrefix = "" }) {
         "request.credentials": "include",
       },
     },
+    // Disable subscriptions (not currently supported with ApolloGateway)
+    subscriptions: false,
   };
 };
