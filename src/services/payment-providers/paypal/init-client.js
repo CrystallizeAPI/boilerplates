f1cb666faf7fbb127c8213d6437c2077e2251392
Håkon Krogh <hakonhk@gmail.com>
2021-07-02 12:59:22 +0200

    Always sandbox env. for paypal for now

--- src/services/payment-providers/paypal/init-client.js
+++ src/services/payment-providers/paypal/init-client.js
@@ -5,10 +5,15 @@ function getClient() {
   const clientSecret =
     process.env.PAYPAL_CLIENT_SECRET || "PAYPAL-SANDBOX-CLIENT-SECRET";
 
-  const clientEnv =
-    process.env.NODE_ENV === "production"
-      ? new checkoutNodeJssdk.core.LiveEnvironment(clientId, clientSecret)
-      : new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
+  // const clientEnv =
+  //   process.env.NODE_ENV === "production"
+  //     ? new checkoutNodeJssdk.core.LiveEnvironment(clientId, clientSecret)
+  //     : new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
+
+  const clientEnv = new checkoutNodeJssdk.core.SandboxEnvironment(
+    clientId,
+    clientSecret
+  );
 
   return new checkoutNodeJssdk.core.PayPalHttpClient(clientEnv);
 }
