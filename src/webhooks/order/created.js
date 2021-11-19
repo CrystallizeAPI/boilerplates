789ec6de44d03699329c18fb06d2355ecf0d7244
Stavros <stavros@snowball.digital>
2021-11-19 10:51:22 +0200

    adding renamed file

--- src/webhooks/order/created.js
+++ src/webhooks/order/created.js
@@ -1,12 +1,17 @@
 const emailService = require("../../services/email-service");
+const handleFBAFulfilment = require("./fulfilment-fba")
 
 module.exports = async function orderCreated(payload) {
   console.log("Webhook payload: orderCreated");
   console.log(JSON.stringify(payload, null, 2));
 
+  const fbaResponse = await handleFBAFulfilment(payload)
+  console.log("FBA Response", JSON.stringify(fbaResponse, null, 2))
   /**
    * You can send out an order confirmation email here
    * if you like
    */
   await emailService.sendOrderConfirmation(payload.order.data.id);
+
+  return fbaResponse
 };
