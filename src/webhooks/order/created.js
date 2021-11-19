afefa35efb1bd7fc6162ac61081cccc49d9434f5
Stavros <stavros@snowball.digital>
2021-11-19 10:29:46 +0200

    fba on order create webhook

--- src/webhooks/order/created.js
+++ src/webhooks/order/created.js
@@ -1,12 +1,20 @@
 const emailService = require("../../services/email-service");
+const handleFBAFulfilment = require("./created-fba")
 
 module.exports = async function orderCreated(payload) {
   console.log("Webhook payload: orderCreated");
   console.log(JSON.stringify(payload, null, 2));
+
+  /*
+  * Fulfil Order using Amazon's FBA
+  */
+  const FBAResponse = await handleFBAFulfilment(payload)
+  console.log("Products resolved", JSON.stringify(FBAResponse, null, 2))
 
   /**
    * You can send out an order confirmation email here
    * if you like
    */
   await emailService.sendOrderConfirmation(payload.order.data.id);
+  return FBAResponse
 };
