2eee03b01e3e606a1beacda9c3d6fa1f318345d4
Dhairya <dhairiya96@gmail.com>
2022-07-05 12:40:45 +0200

    Update cart.tsx

--- components/furnituremix/frontend/src/app/core/components/checkout-forms/cart.tsx
+++ components/furnituremix/frontend/src/app/core/components/checkout-forms/cart.tsx
@@ -19,6 +19,7 @@ export const CheckoutCart: React.FC = () => {
             </div>
         );
     }
+
     return (
         <div className="lg:w-2/5 w-full">
             <h1 className="font-bold text-2xl mt-10 mb-5">Your cart</h1>
@@ -34,7 +35,7 @@ export const CheckoutCart: React.FC = () => {
                                     <Image {...item?.variant.images?.[0]} sizes="100px" />
                                 </div>
                                 <div className="flex flex-col">
-                                    <p className="text-md font-regular w-full">{item.product.name}</p>
+                                    <p className="text-md font-regular w-full">{item.variant.name}</p>
                                     <p className="text-md font-semibold w-full">€{item.price.gross.toFixed(5)}</p>
                                 </div>
                             </div>
