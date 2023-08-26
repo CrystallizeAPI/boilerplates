8d33390c015d7feba20c5a75a175e26188df7483
Dhairya <dhairiya96@gmail.com>
2023-08-26 21:31:41 +0200

    update env vars

--- components/conference/provisioning/clone/setup.bash
+++ components/conference/provisioning/clone/setup.bash
@@ -16,13 +16,12 @@ fi
 # Note that Crystallize CLI is acting before this script.
 
 echo "Setup ${PROJECT_PATH}"
-cp ${PROJECT_PATH}/provisioning/clone/.env.dist ${PROJECT_PATH}/website/.env.dist
 cp ${PROJECT_PATH}/provisioning/clone/robots.txt ${PROJECT_PATH}/website/robots.txt
 cp ${PROJECT_PATH}/provisioning/clone/.env.dist ${PROJECT_PATH}/service-api/.env.dist
 
 #---
 echo "Running command ${PROJECT_PATH}"
-cd ${PROJECT_PATH}/website && cat ${PROJECT_PATH}/website/.env.local.example ${PROJECT_PATH}/website/.env.dist > .env
+cd ${PROJECT_PATH}/website && cp .env.local.example .env
 cd ${PROJECT_PATH}/service-api && cat ${PROJECT_PATH}/service-api/.env.local.example ${PROJECT_PATH}/service-api/.env.dist > .env
 cd ${PROJECT_PATH}/website && npm install --no-progress --no-color --legacy-peer-deps
 cd ${PROJECT_PATH}/service-api && npm install --no-progress --no-color --legacy-peer-deps
