4afb58a1015b390e02211695000df1b23c373430
dhairyadwivedi <dhairiya96@gmail.com>
2024-06-27 12:31:13 +0200

    update credentials

--- components/product-configurator/src/core/credentials.ts
+++ components/product-configurator/src/core/credentials.ts
@@ -1,8 +1,7 @@
 "use sever";
 
 export const credentials = {
-    tenantIdentifier: "product-configurator",
-    tenantId: "6566eee5c53426877c8ad16a",
+    tenantIdentifier: `${process.env.CRYSTALLIZE_TENANT_IDENTIFIER}`,
     accessTokenId: `${process.env.CRYSTALLIZE_ACCESS_TOKEN_ID}`,
     accessTokenSecret: `${process.env.CRYSTALLIZE_ACCESS_TOKEN_SECRET}`,
 } as const;
