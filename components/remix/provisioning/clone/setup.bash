9e8b0f4f40ee667057f30ac70070f1863cbbb1f5
dhairyadwivedi <dhairiya96@gmail.com>
2024-07-18 14:22:29 +0200

    add provisioning

--- components/remix/provisioning/clone/setup.bash
+++ components/remix/provisioning/clone/setup.bash
@@ -16,14 +16,14 @@ fi
 # Note that Crystallize CLI is acting before this script.
 
 echo "Setup ${PROJECT_PATH}"
-cp ${PROJECT_PATH}/provisioning/clone/robots.txt ${PROJECT_PATH}/website/robots.txt
+cp ${PROJECT_PATH}/provisioning/clone/robots.txt ${PROJECT_PATH}/remix/robots.txt
 cp ${PROJECT_PATH}/provisioning/clone/.env.dist ${PROJECT_PATH}/service-api/.env.dist
 
 #---
 echo "Running command ${PROJECT_PATH}"
-cd ${PROJECT_PATH}/website && cat ${PROJECT_PATH}/website/.env.local.example <(echo) ${PROJECT_PATH}/website/.env.dist > .env
+cd ${PROJECT_PATH}/remix && cat ${PROJECT_PATH}/remix/.env.local.example <(echo) ${PROJECT_PATH}/remix/.env.dist > .env
 cd ${PROJECT_PATH}/service-api && cat ${PROJECT_PATH}/service-api/.env.local.example <(echo) ${PROJECT_PATH}/service-api/.env.dist > .env
-cd ${PROJECT_PATH}/website && npm install --no-progress --no-color --legacy-peer-deps
+cd ${PROJECT_PATH}/remix && npm install --no-progress --no-color --legacy-peer-deps
 cd ${PROJECT_PATH}/service-api && npm install --no-progress --no-color --legacy-peer-deps
 
 #---
